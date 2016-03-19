86b0eefbf7740a6920fdb118838db6015bfbfb79
kurt weber <kurt@zipnosis.com>
2016-03-19 14:01:20 -0500

    developed some standards with divs, worked on image further

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ActiveRecord::Base
   belongs_to :user
 
-  has_attached_file :image, styles: { large: "100x100>", medium: "100x100>", thumb: "100x100>" }
+  has_attached_file :image, styles: { large: "800x800>", medium: "300x300>", thumb: "100x100>" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
